b10733b515376ed50039cd0cc042b54be74d0b7f
Hao <xpdinghao@163.com>
2022-04-27 02:13:19 +0200

    add skybox

--- skeleton.py
+++ skeleton.py
@@ -10,8 +10,10 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
+from OpenGL.GL import shaders
 
 import json
+from PIL import Image
 
 FrameSpeed=20
 lengthRatio=500
@@ -35,6 +37,111 @@ lineOrder=(
     (14,15),
     (15,16)
     )
+
+skyBoxFiles_path=(
+    "skybox/skybox/left.jpg",
+    "skybox/skybox/right.jpg",
+    "skybox/skybox/bottom.jpg",
+    "skybox/skybox/top.jpg",
+    "skybox/skybox/back.jpg",
+    "skybox/skybox/front.jpg"
+)
+
+def load_shaders(vert_url, frag_url):
+    vert_str = "\n".join(open(vert_url).readlines())
+    frag_str = "\n".join(open(frag_url).readlines())
+    vert_shader = shaders.compileShader(vert_str, GL_VERTEX_SHADER)
+    frag_shader = shaders.compileShader(frag_str, GL_FRAGMENT_SHADER)
+    program = shaders.compileProgram(vert_shader, frag_shader)
+    return program
+
+def render():
+    global width, height, program
+    global rotation, cubemap
+    global skybox_vbo
+
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_TEXTURE_2D)
+    glEnable(GL_TEXTURE_CUBE_MAP)
+
+    skyratio=1
+
+    skybox_right = [1, -1, -1, 1, -1,  1, 1,  1,  1, 1,  1,  1, 1,  1, -1, 1, -1, -1]
+    skybox_left = [-1, -1,  1, -1, -1, -1, -1,  1, -1, -1,  1, -1, -1,  1,  1, -1, -1,  1]
+    skybox_top = [-1,  1, -1, 1,  1, -1, 1,  1,  1, 1,  1,  1, -1,  1,  1, -1,  1, -1]
+    skybox_bottom = [-1, -1, -1, -1, -1,  1, 1, -1, -1, 1, -1, -1, -1, -1,  1, 1, -1,  1]
+    skybox_back = [-1,  1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, 1,  1, -1, -1,  1, -1]
+    skybox_front = [-1, -1,  1, -1,  1,  1, 1,  1,  1, 1,  1,  1, 1, -1,  1, -1, -1,  1]
+
+    skybox_vertices = np.array([skybox_right, skybox_left, skybox_top, skybox_bottom, skybox_back, skybox_front], dtype=np.float32).flatten()
+    skybox_vbo = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, skybox_vbo)
+    glBufferData(GL_ARRAY_BUFFER, skybox_vertices.nbytes, skybox_vertices, GL_STATIC_DRAW)
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
+
+    glClear(GL_COLOR_BUFFER_BIT)
+    glClear(GL_DEPTH_BUFFER_BIT)
+
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(60, float(width)/height, 0.1, 1000)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    # glRotate(rotation, 0, 1, 0)#spin around y axis
+    glRotate(rotation, 1, 0, 0)#spin around x axis
+    # glRotate(rotation, 1, 1, 1)#rotate around x, y, and z axes
+
+    glUseProgram(program)
+    glDepthMask(GL_FALSE)
+    glBindTexture(GL_TEXTURE_CUBE_MAP, cubemap)
+    glEnableClientState(GL_VERTEX_ARRAY)
+    glBindBuffer(GL_ARRAY_BUFFER, skybox_vbo)
+    glVertexPointer(3, GL_FLOAT, 0, None)
+    glDrawArrays(GL_TRIANGLES, 0, 36)
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
+    glDisableClientState(GL_VERTEX_ARRAY)
+    glBindTexture(GL_TEXTURE_CUBE_MAP, 0)
+    glDepthMask(GL_TRUE)
+    glUseProgram(0)
+
+    # pygame.display.flip()
+
+# def load_cubemap(folder_url):
+def load_cubemap():
+    tex_id = glGenTextures(1)
+    # face_order = ["right", "left", "top", "bottom", "back", "front"]
+    """
+    #hack that fixes issues for ./images1/
+    face_order = ["right", "left", "top", "bottom", "front", "back"]
+    """
+    # face_urls = sorted(glob.glob(folder_url + "*"))
+    glActiveTexture(GL_TEXTURE0)
+    glBindTexture(GL_TEXTURE_CUBE_MAP, tex_id)
+    for i, face in enumerate(skyBoxFiles_path):
+        # face_url = [face_url for face_url in face_urls if face in face_url.lower()][0]
+        face_image = Image.open(face)
+        """
+        #hack that fixes issues for ./images2/
+        if face == "bottom":
+            face_image = pygame.transform.rotate(face_image, 270)
+        if face == "top":
+            face_image = pygame.transform.rotate(face_image, 90)
+        """
+        """
+        #hack that fixes issues for ./images3/
+        if face == "bottom" or face == "top":
+            face_image = pygame.transform.rotate(face_image, 180)
+        """
+        # face_surface = pygame.image.tostring(face_image, 'RGB')
+        face_width, face_height, face_surface = face_image.size[0],face_image.size[1],face_image.tobytes("raw", "RGBX",0,-1)
+        glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, 3, face_width, face_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, face_surface)
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE)
+    glBindTexture(GL_TEXTURE_CUBE_MAP, 0)
+    return tex_id
 
 def cross_product_3(a,b):  
     c = [a[1]*b[2] - a[2]*b[1],
@@ -170,17 +277,17 @@ def getInput(run):
 # coordinate (Draw the coordinate)
 def Coord():
     glBegin(GL_LINES)
-    glColor4f(1, 0, 0, 1)  # Select color
+    glColor4f(1, 0, 0, 1)  # Select color RED, Z
     glVertex3fv(verticies[0])
     glVertex3fv(verticies[1])
     glEnd()
     glBegin(GL_LINES)
-    glColor4f(0, 1, 0, 1)  # Select color
+    glColor4f(0, 1, 0, 1)  # Select color Green, Y
     glVertex3fv(verticies[0])
     glVertex3fv(verticies[2])
     glEnd()
     glBegin(GL_LINES)
-    glColor4f(0, 0, 1, 1)  # Select color
+    glColor4f(0, 0, 1, 1)  # Select color Blue, X
     glVertex3fv(verticies[0])
     glVertex3fv(verticies[3])
     glEnd()
@@ -197,14 +304,21 @@ def printmatrix4(templist):
     pass
 
 viewMatrix = initView(800, 600)
+(width, height) = (800, 600)
 # initView(800, 600)
 sphere = gluNewQuadric()  # Create new sphere
 cylinder = gluNewQuadric()  # Create new cylinder
 
+program=load_shaders("./shaders/skybox.vert", "./shaders/skybox.frag")
+cubemap=load_cubemap()
+rotation=0
 run = [True, 0]
 while run:
     idx=0
     while idx < 370:
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # Clear the screen
+
+        render()
         # s = "%02d" % idx
         s = str(idx)
         nm='animation/'+ s +'.json'
@@ -255,10 +369,6 @@ while run:
 
         # drawCylinder(cylinder,0.5,0.5,1,0,0,0)
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # Clear the screen
-
-
-
         # with open('animation/100.json') as f:
         #     skeleton = json.load(f)
 
@@ -281,10 +391,12 @@ while run:
         # drawSphere(sphere, 1.5, 0, 0)
 
         # glRotatef(1, 0, 1, 0)
+        rotation+=1
 
         pygame.display.flip()  # Update the screen
         pygame.time.wait(FrameSpeed)
 
         idx=idx+1
 
+glDeleteBuffers()
 pygame.quit()
